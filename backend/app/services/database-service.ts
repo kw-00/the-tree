2e0a21ff1b0b591956e6e1821289b8b4cd88bcbb
kw-00 <christopher.p.wojcik@gmail.com>
2025-12-01 19:06:01 +0100

    Registration/authentication service

--- backend/app/services/database-service.ts
+++ backend/app/services/database-service.ts
@@ -1,12 +1,41 @@
+import { Pool } from "pg";
 import DatabaseInterface from "./database-interface";
+import { AccessTokenManagement } from "../utilities/access-token-management";
 
-
+type DatabaseServiceResponse = {
+    httpStatus: number,
+    status: string,
+    message: string
+} & any
 
 
 export default class DatabaseService {
-    databaseInterface: DatabaseInterface
+    dbi: DatabaseInterface
 
-    constructor(databaseInterface: DatabaseInterface) {
-        this.databaseInterface = databaseInterface
+    constructor(pool: Pool) {
+        this.dbi = new DatabaseInterface(pool)
+    }
+
+    async registerUser(login: string, password: string): Promise<DatabaseServiceResponse> {
+        const registrationResult = await this.dbi.registerUser(login, password)
+        return registrationResult
+    }
+
+    async authenticateUser(login: string, password: string): Promise<DatabaseServiceResponse> {
+        const authenticationResult = await this.dbi.authenticateUser(login, password)
+        if (authenticationResult.httpStatus === 200) {
+            const userId = authenticationResult.userId!
+            const refreshTokenCreationResult = await this.dbi.createRefreshToken(userId, Number(process.env.REFRESH_TOKEN_VALIDITY_PERIOD))
+            if (refreshTokenCreationResult.httpStatus === 200) {
+                return {
+                    ...refreshTokenCreationResult,
+                    accessToken: AccessTokenManagement.getToken(userId)
+                }
+            } else {
+                return refreshTokenCreationResult
+            }
+        } else {
+            return authenticationResult
+        }
     }
 }
