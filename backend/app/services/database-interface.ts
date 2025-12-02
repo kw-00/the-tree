c9b6899a6710fc55c030152d0c4e0fcba62ae010
kw-00 <christopher.p.wojcik@gmail.com>
2025-12-02 15:08:40 +0100

    Unexport private function

--- backend/app/services/database-interface.ts
+++ backend/app/services/database-interface.ts
@@ -52,7 +52,7 @@ const databaseCredentials = {
 
 const pool = new Pool(databaseCredentials)
 
-export async function callAPI(functionName: string, ...parameters: any[]): Promise<StandardResponse & any[]> {
+async function _callAPI(functionName: string, ...parameters: any[]): Promise<StandardResponse & any[]> {
     const placeholders = Array.from(parameters.keys())
         .map(v => v + 1)
         .map(v => `$${v}`)
@@ -61,47 +61,47 @@ export async function callAPI(functionName: string, ...parameters: any[]): Promi
     return query.rows[0].result
 }
 export async function registerUser(login: string, password: string): Promise<RegisterUserResponse> {
-    return callAPI("register_user", login, password)
+    return _callAPI("register_user", login, password)
 }
 
 export async function authenticateUser(login: string, password: string): Promise<AuthenticateUserResponse> {
-    return callAPI("authenticate_user", login, password)
+    return _callAPI("authenticate_user", login, password)
 }
 
 export async function verifyRefreshToken(refreshToken: string): Promise<VerifyRefreshTokenResponse> {
-    return callAPI("verify_refresh_token", refreshToken)
+    return _callAPI("verify_refresh_token", refreshToken)
 }
 
 export async function createRefreshToken(userId: number, validityPeriodSeconds: number): Promise<CreateRefreshTokenResponse> {
-    return callAPI("create_refresh_token", userId, validityPeriodSeconds)
+    return _callAPI("create_refresh_token", userId, validityPeriodSeconds)
 }
 
 export async function revokeRefreshToken(refreshToken: string): Promise<StandardResponse> {
-    return callAPI("revoke_refresh_token", refreshToken)
+    return _callAPI("revoke_refresh_token", refreshToken)
 }
 
 export async function addFriend(userId: number, userToBefriendLogin: string, friendshipCode: string): Promise<StandardResponse> {
-    return callAPI("add_friend", userId, userToBefriendLogin, friendshipCode)
+    return _callAPI("add_friend", userId, userToBefriendLogin, friendshipCode)
 }
 
 export async function getFriends(userId: number): Promise<GetFriendsResponse> {
-    return callAPI("get_friends", userId)
+    return _callAPI("get_friends", userId)
 }
 
 export async function addFriendsToChatroom(userId: number, friendIds: number[], chatroomId: number): Promise<AddFriendsToChatroomResponse> {
-    return callAPI("add_friends_to_chatroom", userId, friendIds, chatroomId)
+    return _callAPI("add_friends_to_chatroom", userId, friendIds, chatroomId)
 }
 
 export async function createChatroom(userId: number, chatroomName: string): Promise<CreateChatroomResponse> {
-    return callAPI("create_chatroom", userId, chatroomName)
+    return _callAPI("create_chatroom", userId, chatroomName)
 }
 
 export async function getConnectedChatrooms(userId: number, after: Date): Promise<GetConnectedChatroomsResponse> {
-    return callAPI("get_connected_chatrooms", userId, after)
+    return _callAPI("get_connected_chatrooms", userId, after)
 }
 
 export async function createMessage(userId: number, chatroomId: number, content: string): Promise<StandardResponse> {
-    return callAPI("create_message", userId, chatroomId, content)
+    return _callAPI("create_message", userId, chatroomId, content)
 }
 
 export async function getConversation(
@@ -109,6 +109,6 @@ export async function getConversation(
         before: Date | null, after: Date | null, 
         nRows: number, descending: boolean): Promise<GetConversationResponse> {
 
-    return callAPI("get_conversation", userId, chatroomId, before, after, nRows, descending)
+    return _callAPI("get_conversation", userId, chatroomId, before, after, nRows, descending)
 }
 
