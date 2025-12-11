ed4668aeaf6e7b3cded873112b4755f7f4f6feb6
kw-00 <christopher.p.wojcik@gmail.com>
2025-12-11 11:36:59 +0100

    Move non-sensitive config from .env to .json and create a convenient was to access

---
+++ backend/app/config.ts
@@ -0,0 +1,41 @@
+import raw from "@/config.json" with {type: "json"}
+
+
+import z, { maxLength, minLength } from "zod"
+
+
+const Schema = z.object({
+    tokens: z.object({
+        access: z.object({
+            validityPeriod: z.int().positive()
+        }),
+        refresh: z.object({
+            validityPeriod: z.int().positive()
+        })
+    }),
+    dataRules: z.object({
+        login: z.object({
+            minLength: z.int().positive(),
+            maxLength: z.int().positive()
+        }),
+        password: z.object({
+            minLength: z.int().positive(),
+            maxLength: z.int().positive()
+        }),
+        friendshipCode: z.object({
+            minLength: z.int().positive(),
+            maxLength: z.int().positive()
+        }),
+        chatroomName: z.object({
+            minLength: z.int().positive(),
+            maxLenght: z.int().positive()
+        }),
+        message: z.object({
+            minLength: z.int().positive(),
+            maxLength: z.int().positive()
+        })
+    })
+})
+
+export const Config = await Schema.parseAsync(raw)
+
