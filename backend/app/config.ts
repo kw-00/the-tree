0841de1f499b78ba18a35462a9258b242d910feb
kw-00 <christopher.p.wojcik@gmail.com>
2025-12-11 17:53:27 +0100

    Add endpoint routes to config

--- backend/app/config.ts
+++ backend/app/config.ts
@@ -1,10 +1,41 @@
 import raw from "@/config.json" assert {type: "json"}
 
 
-import z, { maxLength, minLength } from "zod"
+import z from "zod"
 
 
 const Schema = z.object({
+    api: z.object({
+        basePath: z.string().startsWith("/"),
+        auth: z.object({
+            logIn: z.string().startsWith("/"),
+            refreshToken: z.string().startsWith("/"),
+            logOut: z.string().startsWith("/")
+        }),
+        user: z.object({
+            registerUser: z.string().startsWith("/"),
+            changeLogin: z.string().startsWith("/"),
+            changePassword: z.string().startsWith("/")
+        }),
+        friends: z.object({
+            createFriendshipToken: z.string().startsWith("/"),
+            getFriendshipCodes: z.string().startsWith("/"),
+            revokeFriendshipCode: z.string().startsWith("/"),
+            addFriend: z.string().startsWith("/"),
+            getFriends: z.string().startsWith("/"),
+            removeFriend: z.string().startsWith("/")
+        }),
+        chatrooms: z.object({
+            createChatroom: z.string().startsWith("/"),
+            getConnectedChatrooms: z.string().startsWith("/"),
+            addFriendsToChatroom: z.string().startsWith("/"),
+            leaveChatroom: z.string().startsWith("/")
+        }),
+        messages: z.object({
+            createMessage: z.string().startsWith("/"),
+            getMessages: z.string().startsWith("/")
+        })
+    }),
     tokens: z.object({
         access: z.object({
             validityPeriod: z.int().positive()
@@ -30,7 +61,7 @@ const Schema = z.object({
             minLength: z.int().positive(),
             maxLenght: z.int().positive()
         }),
-        message: z.object({
+        messageContent: z.object({
             minLength: z.int().positive(),
             maxLength: z.int().positive()
         })
