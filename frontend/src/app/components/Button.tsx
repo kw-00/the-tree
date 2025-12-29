8a97a946fe9fd520ad3adc545d0a8f69746e0b9e
kw-00 <christopher.p.wojcik@gmail.com>
2025-12-29 20:42:08 +0100

    Move Button styling to CSS file

--- frontend/src/app/components/Button.tsx
+++ frontend/src/app/components/Button.tsx
@@ -1,32 +1,17 @@
+import "./Button.css"
+
 import { twMerge } from "tailwind-merge"
 
 
-
-const styling = {
-    base: `
-        px-6 py-2 rounded-xs button-text
-        hover:brightness-(--hover-brightness) 
-        active:brightness-(--active-brightness)
-    `,
-    variant: {
-        primary: "bg-(--button-bg)",
-        secondary: "bg-(--button-secondary-bg)",
-        ghost: "hover:invert-40 active:invert-50",
-        warning: "bg-(--button-warning-bg)",
-        danger: "bg-(--button-danger-bg)"
-    }
-}
-
 export type ButtonProps = {
-    variant: keyof typeof styling["variant"]
+    variant: "primary" | "secondary" | "ghost" | "danger" | "warning"
 } & React.HTMLAttributes<HTMLButtonElement>
 
-export default function Button({variant, className, ...rest}: ButtonProps) {
-    const classes = twMerge(styling.base, styling.variant[variant], className)
+export default function Button({className, ...rest}: ButtonProps) {
 
 
     return (
-        <button className={classes} {...rest}/>
+        <button className={`Button ${className}`} {...rest}/>
     )
 }
 
