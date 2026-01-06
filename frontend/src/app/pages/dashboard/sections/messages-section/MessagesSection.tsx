c4fa389d905d630d4bc2cefd9dc6d7de6334e67e
kw-00 <christopher.p.wojcik@gmail.com>
2026-01-06 17:17:41 +0100

    Collapse default styling (base.css)

--- frontend/src/app/pages/dashboard/sections/messages-section/MessagesSection.tsx
+++ frontend/src/app/pages/dashboard/sections/messages-section/MessagesSection.tsx
@@ -3,6 +3,7 @@ import TextInput from "@/app/components/text-input/TextInput"
 import Guard from "@/app/layout/Guard"
 
 import { useState } from "react"
+import { useDashboardState } from "../../DashboardState"
 
 
 
@@ -18,24 +19,55 @@ export default function MessagesSection({className, ...rest}: React.HTMLAttribut
         return ms
     })
 
+    const [friends,] = useState(() => {
+        const fs = []
+        while (fs.length < 50) {
+            fs.push(["Frank", "Bob", "Martin", "Chris", "Matt", "Elliot"][Math.floor(Math.random() * 6)])
+        }
+        return fs
+    })
+
+    const state = useDashboardState((state) => [state.layout.show.usersInChatroom])
+    const show = state.layout.show
+
     return (
         <div className={`v-stack ${className ?? ""}`} {...rest}>
             {/* Header */}
             <div className="surface-elevated">
-                <span className="heading-2">Hog Rider Gang</span>
+                <h2 className="heading-2">Hog Rider Gang</h2>
             </div>
-            {/* Messages */}
-            <Guard>
-                <div className="v-stack overflow-y-auto surface-sunken grow">
-                    {messages.map((m, n) => <div key={n} className="surface-elevated">{m}</div>)}
+            <div className="v-stack grow">
+                <div className="h-stack grow">
+                    {/* Messages */}
+                    <div className="v-stack grow basis-5/6">
+                        <Guard>
+                            <div className="v-stack overflow-y-auto surface-sunken grow">
+                                {messages.map((m, n) => <div key={n} className="surface-elevated">{m}</div>)}
+                            </div>
+                        </Guard>
+                    </div>
+
+                    {show.usersInChatroom.get() &&
+                    <div className="v-stack grow">
+                        <div className="surface-elevated">
+                            <h3 className="heading-3">Chatroom members</h3>
+                        </div>
+                        <Guard>
+                            <div className="v-stack overflow-y-auto surface-sunken grow">
+                                {friends.map((f, n) => <div key={n} className="surface-elevated">{f}</div>)}
+                            </div>
+                        </Guard>
+                    </div>}
                 </div>
-            </Guard>
 
                 <div className="v-stack surface-sunken gap-1">
                     <TextInput className={`${styles["message-input"]} input grow`}></TextInput>
                     <button className={`button-primary ${styles["send-button"]}`}>Send</button>
                 </div>
+            </div>
+
         </div>
+
     )
 
 }
