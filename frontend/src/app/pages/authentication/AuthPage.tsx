ca093026f13db9d79459a1991ec8729fff8d5f20
kw-00 <christopher.p.wojcik@gmail.com>
2026-01-07 14:57:56 +0100

    Make auth buttons navigate to dashboard for prototyping purposes

--- frontend/src/app/pages/authentication/AuthPage.tsx
+++ frontend/src/app/pages/authentication/AuthPage.tsx
@@ -1,52 +1,65 @@
 import Label from "@/app/components/label/Label";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
-const LogInForm = () => (
-    <>
-        <h2 className="heading-2 self-center">Log in</h2>
-        <form className="v-stack surface-elevated">
-            <fieldset className="v-stack gap-8 brightness-150">
-                <Label className="v-stack gap-2">
-                    Login
-                    <input className="input grow"></input>
-                </Label>
 
-                <Label className="v-stack gap-2">
-                    Password
-                    <input type="password" className="input grow"></input>
-                </Label>
-            </fieldset>
-            <div className="px-5 py-3 self-end">
-                <button className="button-primary self-end"onClick={e => e.preventDefault()}>Log in</button>
-            </div>
-        </form>
-        <span className="self-center">New here? <Link to="/register" className="link">Register an account</Link></span>
-    </>
-)
+const LogInForm = () => {
+    const navigate = useNavigate()
+    return (
+        <>
+            <h2 className="heading-2 self-center">Log in</h2>
+            <form className="v-stack surface-elevated">
+                <fieldset className="v-stack gap-xl brightness-150">
+                    <Label className="v-stack gap-2">
+                        Login
+                        <input className="input grow"></input>
+                    </Label>
 
-const RegisterForm = () => (
-    <>
-        <h2 className="heading-2 self-center">Register an account</h2>
-        <form className="v-stack surface-elevated">
-            <fieldset className="v-stack gap-sm brightness-150">
-                <Label className="v-stack gap-sm">
-                    Login
-                    <input className="input grow"></input>
-                </Label>
+                    <Label className="v-stack gap-2">
+                        Password
+                        <input type="password" className="input grow"></input>
+                    </Label>
+                </fieldset>
+                <div className="px-5 py-3 self-end">
+                    <button className="button-primary self-end"onClick={e => {
+                        e.preventDefault()
+                        navigate("/dashboard")
+                    }}>Log in</button>
+                </div>
+            </form>
+            <span className="self-center">New here? <Link to="/register" className="link">Register an account</Link></span>
+        </>
+    )
+}
 
-                <Label className="v-stack gap-sm">
-                    Password
-                    <input type="password" className="input grow"></input>
-                </Label>
-            </fieldset>
-            <div className="px-5 py-3 self-end">
-                <button className="button-primary"onClick={e => e.preventDefault()}>Register account</button>
-            </div>
-        </form>
-        <span className="self-center pt-4">Already registered? <Link to="/login" className="link">Log in</Link></span>
-    </>
-)
+const RegisterForm = () => {
+    const navigate = useNavigate()
+    return (
+        <>
+            <h2 className="heading-2 self-center">Register an account</h2>
+            <form className="v-stack surface-elevated">
+                <fieldset className="v-stack gap-xl brightness-150">
+                    <Label className="v-stack gap-sm">
+                        Login
+                        <input className="input grow"></input>
+                    </Label>
+
+                    <Label className="v-stack gap-sm">
+                        Password
+                        <input type="password" className="input grow"></input>
+                    </Label>
+                </fieldset>
+                <div className="px-5 py-3 self-end">
+                    <button className="button-primary"onClick={e => {
+                        e.preventDefault()
+                        navigate("/dashboard")
+                    }}>Register account</button>
+                </div>
+            </form>
+            <span className="self-center">Already registered? <Link to="/login" className="link">Log in</Link></span>
+        </>
+    )
+}
 
 
 
