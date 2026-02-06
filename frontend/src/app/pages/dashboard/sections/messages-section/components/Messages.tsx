baeb5018ac77ab2ff17e52820c50c0f8ef1a035f
kw-00 <christopher.p.wojcik@gmail.com>
2026-02-06 14:34:41 +0100

    Implement chunkification in message component

--- frontend/src/app/pages/dashboard/sections/messages-section/components/Messages.tsx
+++ frontend/src/app/pages/dashboard/sections/messages-section/components/Messages.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react"
-import { useForceUpdate } from "@/app/hooks/ForceUpdate"
+import { useRef } from "react"
 import { getScrollState, type ScrollState } from "@/utils/element"
 import { useMessageStoreWithChunkifiers } from "@/api/domains/messages/message-store-tools"
+import { type MessageData } from "@/api/domains/messages/messages-service"
 
 
 
@@ -14,42 +14,97 @@ const messageBatchSize = 50
 
 
 export default function Messages() {
-    const forceUpdate = useForceUpdate()
-
     const {messageStore, messageStoreChunkifiers} = useMessageStoreWithChunkifiers()
+    const chunkifier = messageStoreChunkifiers.getCursor(chatroomId)
+
     const scrollableRef = useRef<HTMLDivElement | null>(null)
+    const scrollable = scrollableRef.current
+
+    
+
+    const prevTopMessageRef = useRef<MessageData | null>(null)
+    const prevTopMessage = prevTopMessageRef.current
+
+    const resetScrollbar = () => {
+        if (!scrollable) return
+        let heightOnTop = 0
+        const messageNodes = scrollable.children
+        for (const messageNode of messageNodes) {
+            if (Number(messageNode.getAttribute("data-message-id")) === (prevTopMessage?.id ?? -1)) {
+                break
+            }
+            heightOnTop += messageNode.clientHeight
+        }
+        scrollable.scrollTop += heightOnTop
+    }
+    
+    const loadMessageChunk = async (direction: "next" | "previous") => {
+        if (!scrollable) return
+        if (!chunkifier) return
+
+        const topMessage = chunkifier.getChunkUnderCursor()?.[0]?.[0] ?? null
+
+        if (direction === "previous") {
+            if (!chunkifier.firstChunkReached()) {
+                chunkifier.move(-1)
+                resetScrollbar()
+    
+            } else {
+                const currentChunkMessageCount = () => chunkifier.getChunkUnderCursor()?.reduce((prev, next) => prev + next.length, 0) ?? 0
+                const oldChunkSize = currentChunkMessageCount()
+                
+                await messageStore.fetchPreviousMessages(chatroomId, messageBatchSize)
+                const newChunkSize = currentChunkMessageCount()
+                const chunkGrew = newChunkSize - oldChunkSize > 0
+                
+                if (chunkGrew) {
+                    resetScrollbar()
+                } else if (!chunkifier.firstChunkReached()) {
+                    chunkifier.move(-1)
+                    resetScrollbar()
+                }
+            }
+            prevTopMessageRef.current = topMessage
+
+        } else if (direction === "next") {
+            if (!chunkifier.lastChunkReached()) {
+                chunkifier.move()
+                resetScrollbar()
+            }
+        }
+    }
+
+
+
+    
     const prevScrollStateRef = useRef<ScrollState | null>(null)
+    const prevScrollState = prevScrollStateRef.current
 
     const handleScroll = async (e: React.UIEvent<HTMLDivElement>) => {
         e.preventDefault()
-
-        const scrollable = scrollableRef.current
-        if (!scrollable) return
         
+        if (!scrollable) return
+        if (!prevScrollState) return
 
         const currentScrollState = getScrollState(scrollable)
-        const prevScrollState = prevScrollStateRef.current
-        const chunkifier = messageStoreChunkifiers.getCursor(chatroomId)
+        const movedToTop = currentScrollState.isTop && !prevScrollState.isTop
+        const movedToBottom = currentScrollState.isBottom && !prevScrollState.isBottom
 
-        if (prevScrollState && chunkifier) {
-            const movedToTop = currentScrollState.isTop && !prevScrollState.isTop
-            const movedToBottom = currentScrollState.isBottom && !prevScrollState.isBottom
-            const movedNearTop = currentScrollState.isNearTop && !prevScrollState.isNearTop
-            const movedNearBottom = currentScrollState.isNearBottom && !prevScrollState.isNearBottom 
-
-            if (movedToTop) {
-
-            } else if (movedToBottom) {
-
-            }
+        if (movedToTop) {
+            loadMessageChunk("previous")
+        } else if (movedToBottom) {
+            loadMessageChunk("next")
         }
+
         prevScrollStateRef.current = currentScrollState
     }
+
+    const messages = chunkifier?.getChunkUnderCursor()?.reduce((val, c) => [...val, ...c])
 
     return (
         <div className="v-stack grow basis-5/6">
             <div className="v-stack overflow-y-auto surface-sunken grow contain-size" onScroll={handleScroll} ref={scrollableRef}>
-                {messages.map((item, key) => <div key={key} className="surface-item">{item.content}</div>)}
+                {messages?.map((message, key) => <div key={key} data-message-id={message.id} className="surface-item">{message.content}</div>) ?? "Chunkifier not initialized"}
             </div>
         </div>
     )
