570ca0bbf6e5e2930e201d598f337be24cbcbfa0
kw-00 <christopher.p.wojcik@gmail.com>
2026-04-02 14:56:20 +0200

    Create req/res parsers for chatrooms service

--- api/common/zod-schemas.ts
+++ api/common/zod-schemas.ts
@@ -1,14 +1,15 @@
 import z from "zod";
 
-const baseValidators = {
-    timestamptz: z.iso.datetime({offset: true}),
-    serialId: z.int().positive()
-}
+
 
 export class FieldValidators {
+    static base = {
+        timestamptz: z.iso.datetime({offset: true}),
+        serialId: z.int().positive()
+    }
 
     static user = {
-        id: baseValidators.serialId,
+        id: FieldValidators.base.serialId,
         login: z.string().min(1).max(30),
         password: z.string().min(5).max(50)
     }
@@ -19,17 +20,17 @@ export class FieldValidators {
     }
 
     static friendshipCode = {
-        id: baseValidators.serialId,
+        id: FieldValidators.base.serialId,
         code: z.string().min(1).max(50)
     }
 
     static chatroom = {
-        id: baseValidators.serialId,
-        name: z.string().min(1).max(200)
+        id: FieldValidators.base.serialId,
+        name: z.string().min(1).max(200),
     }
 
     static message = {
-        id: baseValidators.serialId,
+        id: FieldValidators.base.serialId,
         content: z.string().min(1).max(5000)
     }
 }
---
+++ api/domains/chatrooms/parsers.ts
@@ -0,0 +1,76 @@
+import z from "zod";
+import { ApiParserFactory } from "../../common/api-parsing";
+import { FieldValidators } from "../../common/zod-schemas";
+
+const models = {
+    chatroomData: z.object({
+        id: FieldValidators.chatroom.id,
+        name: FieldValidators.chatroom.name,
+        joinedAt: FieldValidators.base.timestamptz
+    })
+}
+
+export default class ChatroomsParsers{
+    static createChatroom = {
+        parseRequest: ApiParserFactory.createRequestParser(z.object({
+            userId: FieldValidators.user.id,
+            chatroomName: FieldValidators.chatroom.name
+        })),
+
+        parseResponse: ApiParserFactory.createResponseParser(
+            z.object({
+                chatroomData: models.chatroomData
+            }),
+            ["SUCCESS", "NOT_FOUND"]
+        )
+    }
+
+    static getChatrooms = {
+        parseRequest: ApiParserFactory.createRequestParser(z.object({
+            userId: FieldValidators.user.id,
+            after: FieldValidators.base.timestamptz
+        })),
+
+        parseResponse: ApiParserFactory.createResponseParser(
+            z.object({
+                chatroomsData: z.array(
+                    z.object({
+                        id: FieldValidators.chatroom.id,
+                        name: FieldValidators.chatroom.name,
+                        joinedAt: FieldValidators.base.timestamptz
+                    })
+                )
+            }),
+            ["SUCCESS", "NOT_FOUND"]
+        )
+    }
+
+    static addFriendsToChatroom = {
+        parseRequest: ApiParserFactory.createRequestParser(z.object({
+            userId: FieldValidators.user.id,
+            friendIds: z.array(FieldValidators.user.id),
+            chatroomId: FieldValidators.chatroom.id
+        })),
+
+        parseResponse: ApiParserFactory.createResponseParser(
+            z.object({
+                added: z.array(FieldValidators.user.id),
+                skipped: z.array(FieldValidators.user.id),
+                notFound: z.array(FieldValidators.user.id)
+            }),
+            ["SUCCESS", "NOT_FOUND", "NOT_IN_CHATROOM"]
+        )
+    }
+
+    static leaveChatroom = {
+        parseRequest: ApiParserFactory.createRequestParser(z.object({
+            userId: FieldValidators.user.id,
+            chatroomId: FieldValidators.chatroom.id
+        })),
+
+        parseResponse: ApiParserFactory.createResponseParser(
+            z.object(),
+            ["SUCCESS", "SUCCESS_REDUNDANT", "NOT_FOUND"]
+        )
+    }
+}
